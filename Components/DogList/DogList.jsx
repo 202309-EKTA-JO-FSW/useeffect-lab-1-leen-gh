aff3474b5f94893acaa183ec1e9734e537c95c75
leen-gh <leen.ghaleb.d@gmail.com>
2023-10-30 19:26:46 +0300

    in Class

--- Components/DogList/DogList.jsx
+++ Components/DogList/DogList.jsx
@@ -1,6 +1,23 @@
+import React, { useState, useEffect } from 'react'
+
+export default function ShoppingList() {
+  const [image, setImage] = useState(null)
+  const [isLoading, setIsLoading] = useState(true);
+  
+  useEffect(() => {
+    fetch("https://dog.ceo/api/breeds/image/random/3")
+      .then((res) => res.json())
+      .then((image) => {
+        setImage(image)
+        setIsLoading(false);
+      });
+  }, []);
+  
+  //console.log(image)
+  if (isLoading) return <p>Loading...</p>;
+  if (!image) return <p>No profile data is found</p>;
 
 
-export default function ShoppingList({ dogsList = [] }) {
   return (
     <div className="shopping-cart">
       <h3 style={{ textAlign: "center" }}>
@@ -9,8 +26,11 @@ export default function ShoppingList({ dogsList = [] }) {
           🐶
         </span>
       </h3>
-      {dogsList.map((dog) => (
-        <img src={dog} alt="" />
+      {console.log(image)}
+      {image.message.map((dog) => (
+        <img src={dog
+        } alt="Random Dogs" />
+        
       ))}
     </div>
   );
